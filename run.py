61cbe87bfa27a696c47a5a4a3c56fd2e3152e027
adminlove520 <791751568@qq.com>
2023-10-14 19:46:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import os
 with open('domain.txt','r') as file:
     hosts = [line.strip() for line in file.readlines()]
 for host in hosts:
-    os.system("python3 ParamSpider/paramspider.py --domain "+host+" --output output/output.txt")
+    os.system("python3 ParamSpider/paramspider.py --domain "+host+" --output output.txt")
